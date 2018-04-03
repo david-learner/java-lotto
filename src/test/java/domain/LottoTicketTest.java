ecca74907d570fca97ac7573f67466b021951ef6
David <xodnjs2549@naver.com>
2018-04-03 20:10:31 +0900

    Feat : Add pick bonus ball

--- src/test/java/domain/LottoTicketTest.java
+++ src/test/java/domain/LottoTicketTest.java
@@ -12,12 +12,14 @@ public class LottoTicketTest {
     @Test
     public void getMatchedCountPass() throws Exception {
         List<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
-        List<Integer> list2 = new ArrayList<>(Arrays.asList(1, 2, 3, 7, 8, 9));
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 8, 9));
         LottoTicket lottoTicket = new LottoTicket(list1);
         LottoTicket winningLottoTicket = new LottoTicket(list2);
+
+        // Arrays.asList는 내부적으로 ArrayList를 생성해서 쓴다. 따라서 Collection의 ArrayList랑 다르다.
 //        LottoTicket lottoTicket = new LottoTicket(Arrays.asList(1, 2, 3, 4, 5, 6));
 //        LottoTicket winningLottoTicket = new LottoTicket(Arrays.asList(1, 2, 3, 7, 8, 9));
 
-        assertEquals(3, lottoTicket.getMatchedCount(winningLottoTicket));
+        assertEquals(4, lottoTicket.getMatchedCount(winningLottoTicket));
     }
 }
